979fa0aca002caec3ee9d4130f0fafcbc7ee6e0a
Fergus Cooper <3770306+fcooper8472@users.noreply.github.com>
2024-06-07 16:18:40 +0100

    GSL not required?

--- include/std.h
+++ include/std.h
@@ -20,19 +20,6 @@ using namespace std;
 #include <sys/time.h>
 #include <assert.h>
 #include <iomanip>
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_matrix.h>
-#include <gsl/gsl_vector.h>
-#include <gsl/gsl_linalg.h>
-#include <gsl/gsl_randist.h>
-#include <gsl/gsl_sf_gamma.h>
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_min.h>
-#include <gsl/gsl_multimin.h>
-#include <gsl/gsl_sort.h>
-#include <gsl/gsl_statistics.h>
-#include <gsl/gsl_cdf.h>
 
 #include <getopt.h>
 
